9f4f2b0fb793b6ac9cbc7957f422c776c19684bd
E <uyenla1224@gmail.com>
2026-01-08 15:56:23 -0800

    Fix installer prompts and conditional syntax

--- RUNME.sh
+++ RUNME.sh
@@ -1,11 +1,17 @@
-echo "Local Installer - Installer"
-echo "You will be guided through out this process.\n"
+#!/bin/sh
 
-echo "To install the software, please type (Y/n)"
+echo "Local Installer - Installer"
+echo "You will be guided throughout this process."
+echo
+
+printf "To install the software, please type (Y/n): "
 read install
 
-if [["$install" == "Y"]] then
-  echo "\nYou will now install the Local Installer (li)"
-elif [["$install" == "n"]] then
-  echo "Now exiting the installer."
+if [ "$install" = "Y" ]; then
+    echo
+    echo "You will now install the Local Installer (li)"
+elif [ "$install" = "n" ]; then
+    echo "Now exiting the installer."
+else
+  echo ""
 fi
