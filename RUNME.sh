2da78684367ace6165fe653d673df31c7e405e1b
E <uyenla1224@gmail.com>
2026-01-08 15:54:45 -0800

    Implement local installer prompts in RUNME.sh
    Added a local installer script with prompts for installation.

--- RUNME.sh
+++ RUNME.sh
@@ -1,1 +1,11 @@
-echo "Hello, world!"
+echo "Local Installer - Installer"
+echo "You will be guided through out this process.\n"
+
+echo "To install the software, please type (Y/n)"
+read install
+
+if [["$install" == "Y"]] then
+  echo "\nYou will now install the Local Installer (li)"
+elif [["$install" == "n"]] then
+  echo "Now exiting the installer."
+fi
